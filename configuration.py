3b7e51dc6dc889345990f9e2924aca5801f47afa
novdima1 <dnavadvorski@intelliflo.com>
2023-02-20 00:24:51 +0100

    Add Response class

--- configuration.py
+++ configuration.py
@@ -1,1 +1,1 @@
-SERVICE_URL = "https://my-json-server.typicode.com/typicode/demo/posts"
+TYPICODE_URL = "https://my-json-server.typicode.com/typicode/demo/posts"
--- tests/test_smth.py
+++ tests/test_smth.py
@@ -1,15 +1,13 @@
 # https://my-json-server.typicode.com/typicode/demo/posts
 import requests
-from configuration import SERVICE_URL
-from src.enums.global_enums import GlobalErrorMessages
+from configuration import TYPICODE_URL
+from src.schemas.post import POST_SCHEMA
+from src.baseclasses.response import Response
 
-def test_python_site():
-    r = requests.get('https://www.python.org')
-    assert r.status_code == 200
-    assert b'Python is a programming language' in r.content
 
-def test_getting_post():
-    response = requests.get(url=SERVICE_URL)
-    assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
-    received_posts = response.json()
-    assert len(received_posts) == 3, GlobalErrorMessages.WRONG_ELEMENT_COUNT
+def test_getting_posts():
+    r = requests.get(TYPICODE_URL)
+    response = Response(r)
+    response.assert_status_code(200).validate(POST_SCHEMA)
+
+
