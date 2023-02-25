c5c5eaa7916838878bcb671b39d0f0ce01fb1e33
novdima1 <dnavadvorski@intelliflo.com>
2023-02-25 17:33:43 +0100

    Add basic fixture

--- tests/test_smth.py
+++ tests/test_smth.py
@@ -1,17 +1,11 @@
-# https://my-json-server.typicode.com/typicode/demo/posts
-import requests
-from configuration import GOREST_USERS
-from src.schemas.post import POST_SCHEMA
 from src.baseclasses.response import Response
 from src.pydantic_schemas.user import User
 
 
-def test_getting_posts():
-    r = requests.get(GOREST_USERS)
-    response = Response(r)
-    response.assert_status_code(200).validate(User) # POST_SCHEMA
+def test_getting_posts(get_users):
+    Response(get_users).assert_status_code(200).validate(User) # POST_SCHEMA
 
-def test_get_responce_json():
-    r = requests.get(GOREST_USERS)
-    response = Response(r)
+
+def test_get_response_json(get_users):
+    response = Response(get_users)
     response.print_response_json()
